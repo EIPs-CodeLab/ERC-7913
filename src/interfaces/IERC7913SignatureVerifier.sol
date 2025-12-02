ae4e213b1ce05a20ff2e13c5bc1855ffc9e66e40
zacksfF <zakaria.saiff@gmail.com>
2025-12-02 15:24:05 +0100

    interface::IERC7913SignatureVerifier

--- src/interfaces/IERC7913SignatureVerifier.sol
+++ src/interfaces/IERC7913SignatureVerifier.sol
@@ -8,6 +8,24 @@ pragma solidity ^0.8.20;
  */
 interface IERC7913SignatureVerifier {
 
+     /**
+     * @dev Verifies `signature` as a valid signature of `hash` by `key`.
+     *
+     * @param key The public key in bytes format (format depends on the cryptographic scheme)
+     * @param hash The hash of the message that was signed (typically EIP-191 or EIP-712)
+     * @param signature The signature to verify
+     *
+     * @return bytes4 Returns 0x024ad318 (this function's selector) if signature is valid
+     *                Returns 0xffffffff or reverts if signature is invalid
+     *                Should revert if key is empty
+     *
+     * Requirements:
+     * - MUST return 0x024ad318 if the signature is valid
+     * - SHOULD return 0xffffffff or revert if signature is invalid
+     * - SHOULD revert if key is empty
+     * - SHOULD be a view/pure function (stateless)
+     */
+
     function verify(
         bytes calldata key,
         bytes32 hash, 
